3703f356459e8f083bcfa28648c0c6a4c792fe8f
Jdfxbb <jdfxbb@mail.umkc.edu>
2019-12-18 13:39:43 -0600

    Studio-2 + bonus

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipment: string[] = [
+    'Habitat Dome',
+    'Drones',
+    'Food Containers',
+    'Oxygen Tanks'
+  ];
+  itemBeingEdited: string = null;
   constructor() { }
 
   ngOnInit() {
   }
+
+  add(itemName: string) {
+    if(this.equipment.findIndex( item => item === itemName) === -1){
+      this.equipment.push(itemName);
+    }
+  }
+  remove(item: string) {
+    let index = this.equipment.indexOf(item);
+    this.equipment.splice(index, 1);
+  }
+  edit(item: string) {
+    this.itemBeingEdited = item;
+  }
+  save(item: string) {
+    this.equipment[this.equipment.indexOf(this.itemBeingEdited)] = item;
+    this.itemBeingEdited = null;
+  }
+
 
 }
--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentsComponent implements OnInit {
 
+  experiments: string[] = [
+    'Mars Soil Sample',
+    'Plant Growth in Habitat',
+    'Human Bone Density'
+  ];
+  experimentBeingEdited: string = null;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  add(experimentName: string) {
+    if(this.experiments.findIndex( experiment => experiment === experimentName) === -1){
+      this.experiments.push(experimentName);
+    }
+  }
+  remove(experiment: string) {
+    let index = this.experiments.indexOf(experiment);
+    this.experiments.splice(index, 1);
+  }
+  edit(experiment: string) {
+    this.experimentBeingEdited = experiment;
+  }
+  save(experiment: string) {
+    this.experiments[this.experiments.indexOf(this.experimentBeingEdited)] = experiment;
+    this.experimentBeingEdited = null;
   }
 
 }
